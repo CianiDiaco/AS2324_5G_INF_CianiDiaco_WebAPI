e4add1770dce8332069392d434a95365571d1575
Diaco <cianidiaco@gmail.com>
2024-05-08 08:32:56 +0200

    completamento metodo calcolo multiplo intero

---
+++ Controllers/MathController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_CianiDiaco_WebAPI.Controllers
+{
+    public class MathController : Controller
+    {
+
+        [HttpGet("MultiploIntero")]
+
+        public JsonResult MultiploIntero(int numero, int divisore)
+        {
+            string status_result = "OK";
+            string message = "é un multiplo intero";
+            if (numero == 0)
+            {
+                status_result = "KO";
+            }
+            else if (numero % divisore != 0)
+            {
+                message = "il numero inaserito non è un multiplo intero";
+            }
+            else
+            {
+                message = "é un multiplo intero";
+            }
+            return Json(new {output = message, status = status_result});
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
